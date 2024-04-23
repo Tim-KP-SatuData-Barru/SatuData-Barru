42db5b8ed502272b66ae65b40f738621ba8dea6a
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-04-23 12:54:08 +0800

    modify some code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,54 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+          "blue-dark": "#213C5D",
+          blue: "#0E5383",
+          "blue-light": "#5189B1",
+          "orange-dark": "#FF9519",
+          orange:"#FB7436",
+          "orange-light":"#FBA834",
+          black: "#000000",
+          white: "#FFFFFF",
+          "white-hover": "#cecece",
+      },
+      fontSize: {
+        "text-s": [
+          "12px",
+          {
+            lineHeight: "16px",
+          },
+        ],
+        "text-m": [
+          "14px",
+          {
+            lineHeight: "20px",
+          },
+        ],
+        "text-l": [
+          "16px",
+          {
+            lineHeight: "24px",
+          },
+        ],
+        "heading-s": [
+          "20px",
+          {
+            lineHeight: "28px",
+          },
+        ],
+        "heading-m": [
+          "28px",
+          {
+            lineHeight: "36px",
+          },
+        ],
+        "heading-l": [
+          "36px",
+          {
+            lineHeight: "44px",
+          },
+        ],
       },
     },
   },
