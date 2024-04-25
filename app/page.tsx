71e983d2f8460a6c03b88feb93dd3d9d080ff42a
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-04-25 18:00:03 +0800

    add statisitk

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "./components/footer";
 import InfoAktualDashboard from "./components/infoAktualDashboard";
 import Navbar from "./components/navbar";
+import Statistik from "./components/statistik";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Statistik />
       <InfoAktualDashboard />
       <Footer />
     </main>
