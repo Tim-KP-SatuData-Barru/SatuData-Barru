ee356e953bcfc3d93a6b72d8a3007e93156a7a48
RandyTunru <randytunru@gmail.com>
2024-04-30 18:22:01 +0800

    feat: added informasi aktual page
    established connection to page buttons for pagination

---
+++ app/informasi-aktual/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import Footer from "@/app/components/footer";
+import Navbar from "@/app/components/navbar";
+import React from "react";
+import PageButton from "../components/pageButton";
+import InfoCard from "../components/infoCard";
+import { useState } from "react";
+
+const infoAktualItems = [
+  {
+    id: 1,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Konten 1",
+  },
+  {
+    id: 2,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+  },
+  {
+    id: 3,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+  },
+  {
+    id: 4,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Konten 2"
+  },
+  {
+    id: 5,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+  },
+  {
+    id: 6,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+  },
+  {
+    id: 7,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Konten 3",
+  },
+  {
+    id: 8,
+    imgsrc: "/images/data_barru.jpg",
+    tags: ["tag1", "tag2", "tag3"],
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+  },
+]
+
+
+
+function InformasiAktualList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3; // Number of items to display per page
+  const totalPages = Math.ceil(infoAktualItems.length / itemsPerPage);
+
+  // Calculate the start and end indices for the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, infoAktualItems.length);
+
+  // Slice the infoAktualItems array to get the items for the current page
+  const itemsForPage = infoAktualItems.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      <Navbar />
+
+      <section className="flex flex-wrap gap-6 mb-5 p-[5vh] justify-center">
+        {itemsForPage.map((data) => (
+          <InfoCard key={data.id} imgsrc={data.imgsrc} tags={data.tags} content={data.content}/>
+        ))}
+      </section>
+
+      <PageButton currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}/>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default InformasiAktualList;
