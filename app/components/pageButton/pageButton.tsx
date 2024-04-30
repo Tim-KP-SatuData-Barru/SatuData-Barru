77826c53f18f0e227628aace48b587f311809ffb
RandyTunru <randytunru@gmail.com>
2024-04-30 18:36:51 +0800

    refactor: removed unnecessary codes from pageButton
    pageButton now uses the page number and starting page from the page it is called in

--- app/components/pageButton/pageButton.tsx
+++ app/components/pageButton/pageButton.tsx
@@ -9,40 +9,34 @@ interface PageButtonProps {
   
 
 function PageButton({ currentPage, totalPages, onPageChange }: PageButtonProps) {
-  const maxNum = totalPages;
-
-  const [pageNum, setPageNum] = useState(1);
-
+  
   const handleDecrease = () => {
-    setPageNum((prevPageNum) => Math.max(prevPageNum - 1, 1));
     onPageChange(Math.max(currentPage - 1, 1));
   };
 
   const handleIncrease = () => {
-    setPageNum((prevPageNum) => Math.min(prevPageNum + 1, maxNum));
     onPageChange(Math.min(currentPage + 1, totalPages));
   };
 
   const handlePageChange = (number: number) => {
-    setPageNum(number);
     onPageChange(number);
   };
 
   const arrInit = (_: any, index: number) => {
-    if (pageNum === 1) {
+    if (currentPage === 1) {
       return index + 1;
-    } else if (pageNum === maxNum) {
-      return pageNum - 2 + index;
+    } else if (currentPage === totalPages) {
+      return currentPage - 2 + index;
     } else {
-      return pageNum - 1 + index;
+      return currentPage - 1 + index;
     }
   };
 
   return (
     <div className="flex flex-row gap-[20px] justify-center mb-8">
       <button onClick={handleDecrease} className="flex justify-center items-center w-[3vw] h-[3vw] border-2 border-blue-dark bg-white text-blue-dark px-4 py-2 rounded-full">{"<"}</button>
-      {Array.from({ length: 3 }, arrInit).map((number) => number > 0 && number <= maxNum ?  (
-        <button onClick={() => handlePageChange(number)} key={number} className={`flex justify-center items-center w-[3vw] h-[3vw] border-2 border-blue-dark px-4 py-2 rounded-full ${number === pageNum ? 'bg-blue-dark text-white' : 'bg-white text-blue-dark'}`}>
+      {Array.from({ length: 3 }, arrInit).map((number) => number > 0 && number <= totalPages ?  (
+        <button onClick={() => handlePageChange(number)} key={number} className={`flex justify-center items-center w-[3vw] h-[3vw] border-2 border-blue-dark px-4 py-2 rounded-full ${number === currentPage ? 'bg-blue-dark text-white' : 'bg-white text-blue-dark'}`}>
           {number}
         </button>
         ) : null
