8a399a6fbe9ae3c587d334326837b020bd165d7f
RandyTunru <randytunru@gmail.com>
2024-04-30 18:26:15 +0800

    feat: added pagination handling to pageButton
    page button changes can now effect the state of the page

--- app/components/pageButton/pageButton.tsx
+++ app/components/pageButton/pageButton.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { useState } from "react";
 
-function PageButton() {
-  const maxNum = 6;
+interface PageButtonProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+  
+
+function PageButton({ currentPage, totalPages, onPageChange }: PageButtonProps) {
+  const maxNum = totalPages;
 
   const [pageNum, setPageNum] = useState(1);
 
   const handleDecrease = () => {
     setPageNum((prevPageNum) => Math.max(prevPageNum - 1, 1));
+    onPageChange(Math.max(currentPage - 1, 1));
   };
 
   const handleIncrease = () => {
     setPageNum((prevPageNum) => Math.min(prevPageNum + 1, maxNum));
+    onPageChange(Math.min(currentPage + 1, totalPages));
   };
 
   const handlePageChange = (number: number) => {
     setPageNum(number);
+    onPageChange(number);
   };
 
   const arrInit = (_: any, index: number) => {
@@ -29,7 +39,7 @@ function PageButton() {
   };
 
   return (
-    <div className="flex flex-row gap-[20px] justify-center my-[2vh]">
+    <div className="flex flex-row gap-[20px] justify-center mb-8">
       <button onClick={handleDecrease} className="flex justify-center items-center w-[3vw] h-[3vw] border-2 border-blue-dark bg-white text-blue-dark px-4 py-2 rounded-full">{"<"}</button>
       {Array.from({ length: 3 }, arrInit).map((number) => number > 0 && number <= maxNum ?  (
         <button onClick={() => handlePageChange(number)} key={number} className={`flex justify-center items-center w-[3vw] h-[3vw] border-2 border-blue-dark px-4 py-2 rounded-full ${number === pageNum ? 'bg-blue-dark text-white' : 'bg-white text-blue-dark'}`}>
