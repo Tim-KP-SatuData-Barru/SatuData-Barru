8223cc25f9aebcad6b6cf7ba7f97b22f06ee1ab7
RandyTunru <randytunru@gmail.com>
2024-05-13 14:43:56 +0800

    feat added page title and breadcrumbs

--- app/informasi-aktual/page.tsx
+++ app/informasi-aktual/page.tsx
@@ -4,59 +4,9 @@ import Navbar from "@/app/components/navbar";
 import React from "react";
 import PageButton from "../components/pageButton";
 import InfoCard from "../components/infoCard";
+import SearchBar from "../components/searchBar";
 import { useState } from "react";
 import mockDataInfoAktual from "@/public/mockData/mockDataInfoAktual";
-
-// const infoAktualItems = [
-//   {
-//     id: 1,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Konten 1",
-//   },
-//   {
-//     id: 2,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-//   },
-//   {
-//     id: 3,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-//   },
-//   {
-//     id: 4,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Konten 2"
-//   },
-//   {
-//     id: 5,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-//   },
-//   {
-//     id: 6,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-//   },
-//   {
-//     id: 7,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Konten 3",
-//   },
-//   {
-//     id: 8,
-//     imgsrc: "/images/data_barru.jpg",
-//     tags: ["tag1", "tag2", "tag3"],
-//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-//   },
-// ]
 
 
 
@@ -79,6 +29,17 @@ function InformasiAktualList() {
   return (
     <div>
       <Navbar />
+
+      <div className="flex flex-col gap-2 ml-[2vw] mt-[3vh]">
+        <h1 className="text-blue-dark font-bold text-3xl">
+          Informasi Aktual
+        </h1>
+        <p className="text-blue-dark text-base">
+          Home &gt; Informasi Aktual
+        </p>
+      </div>
+
+      <SearchBar />
 
       <section className="flex flex-wrap gap-6 mb-5 p-[5vh] justify-center">
         {itemsForPage.map((data) => (
