5569cfd4c5e0ec42b5fd739bace473e370785f73
RandyTunru <randytunru@gmail.com>
2024-05-13 12:06:14 +0800

    fix: fixed routing from informasi aktual page to id specific page

--- app/informasi-aktual/page.tsx
+++ app/informasi-aktual/page.tsx
@@ -5,71 +5,72 @@ import React from "react";
 import PageButton from "../components/pageButton";
 import InfoCard from "../components/infoCard";
 import { useState } from "react";
+import mockDataInfoAktual from "@/public/mockData/mockDataInfoAktual";
 
-const infoAktualItems = [
-  {
-    id: 1,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Konten 1",
-  },
-  {
-    id: 2,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-  },
-  {
-    id: 3,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-  },
-  {
-    id: 4,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Konten 2"
-  },
-  {
-    id: 5,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-  },
-  {
-    id: 6,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-  },
-  {
-    id: 7,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Konten 3",
-  },
-  {
-    id: 8,
-    imgsrc: "/images/data_barru.jpg",
-    tags: ["tag1", "tag2", "tag3"],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
-  },
-]
+// const infoAktualItems = [
+//   {
+//     id: 1,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Konten 1",
+//   },
+//   {
+//     id: 2,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+//   },
+//   {
+//     id: 3,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+//   },
+//   {
+//     id: 4,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Konten 2"
+//   },
+//   {
+//     id: 5,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+//   },
+//   {
+//     id: 6,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+//   },
+//   {
+//     id: 7,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Konten 3",
+//   },
+//   {
+//     id: 8,
+//     imgsrc: "/images/data_barru.jpg",
+//     tags: ["tag1", "tag2", "tag3"],
+//     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vel orci nec eros ultricies varius. Nulla facilisi. Ut auctor, sapien nec condimentum aliquet, nunc eros ultricies velit, ac fermentum erat purus at libero. Cras vel nunc sed elit tincidunt ultricies. Suspendisse potenti.",
+//   },
+// ]
 
 
 
 function InformasiAktualList() {
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 3; // Number of items to display per page
-  const totalPages = Math.ceil(infoAktualItems.length / itemsPerPage);
+  const itemsPerPage = 6; // Number of items to display per page
+  const totalPages = Math.ceil(mockDataInfoAktual.length / itemsPerPage);
 
   // Calculate the start and end indices for the current page
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, infoAktualItems.length);
+  const endIndex = Math.min(startIndex + itemsPerPage, mockDataInfoAktual.length);
 
   // Slice the infoAktualItems array to get the items for the current page
-  const itemsForPage = infoAktualItems.slice(startIndex, endIndex);
+  const itemsForPage = mockDataInfoAktual.slice(startIndex, endIndex);
 
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
@@ -81,7 +82,7 @@ function InformasiAktualList() {
 
       <section className="flex flex-wrap gap-6 mb-5 p-[5vh] justify-center">
         {itemsForPage.map((data) => (
-          <InfoCard key={data.id} imgsrc={data.imgsrc} tags={data.tags} content={data.content}/>
+          <InfoCard key={data.id} id={data.id} title={data.title} image={data.image} date={data.date} views={data.views} />
         ))}
       </section>
 
