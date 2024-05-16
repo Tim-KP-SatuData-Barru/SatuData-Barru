4f2fb03f40b5a686886f90e32f2c91af2899e19a
RandyTunru <randytunru@gmail.com>
2024-05-17 01:20:29 +0800

    feat: finished DatasetCard component used in dataset page

--- app/components/DatasetCard/DatasetCard.tsx
+++ app/components/DatasetCard/DatasetCard.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import textOverflow from "@/lib/textOverflow";
+import { DatasetProps } from "@/public/types/DatasetTypes";
 
 interface DatasetCardProps {
-  key: string,
-  id: string,
-  judul: string,
-  tanggal: string,
-  dinas: string,
-  jenis: string,
+  card: DatasetProps,
+  onClick: (card: DatasetProps) => void,
 }
 
 function DatasetCard(data: DatasetCardProps) {
@@ -24,24 +21,24 @@ function DatasetCard(data: DatasetCardProps) {
   }
 
   return (
-    <div className="flex flex-row border bg-white p-5 mx-16">
+    <div className="flex flex-row border bg-white p-5 mx-16" onClick={() => data.onClick(data.card)}>
       <Image 
         alt="logo" 
-        src={getJenisLogo(data.jenis) as string}
+        src={getJenisLogo(data.card.jenis) as string}
         width={80}
         height={80}
       />
       <div className="w-[100%] mx-5 flex flex-col gap-3">
-        <h1>{data.judul}</h1>
+        <h1>{data.card.judul}</h1>
         <div className="h-[1px] border w-[100%] "></div>
         <div className="flex flex-row gap-5">
           <div className="flex flex-row bg-orange rounded-2xl px-2 py-1 w-[20%] justify-center items-center gap-2">
             <Image alt="logo" src="/images/dataset/TimelineWeek.png" width={15} height={15} />
-            <p className="text-white text-xs">{data.tanggal}</p>
+            <p className="text-white text-xs">{data.card.tanggal}</p>
           </div>
           <div className="flex flex-row bg-lime-500 rounded-2xl px-2 py-1 w-[20%] justify-center gap-2">
             <Image alt="logo" src="/images/dataset/PriceTag.png" width={15} height={15} />
-            <p className="text-white text-xs">{data.dinas}</p>
+            <p className="text-white text-xs">{data.card.dinas}</p>
           </div>
         </div>
       </div>
