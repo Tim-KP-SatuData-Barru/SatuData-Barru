a9a8df10044d5b544c470f948d900b4d90cfc5a8
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-05-21 15:15:17 +0800

    feat: added mock data

---
+++ public/mockData/mockDataSektoral.ts
@@ -0,0 +1,22 @@
+export const dataSektoralItems = [
+    {
+        id: 1,
+        title: "Data Sektoral",
+        imgsrc: "/images/data_barru.jpg"
+    },
+    {
+        id: 2,
+        title: "Survey Sektoral",
+        imgsrc: "/images/data_barru.jpg"
+    },
+    {
+        id: 3,
+        title: "Kompilasi Produk Administrasi",
+        imgsrc: "/images/data_barru.jpg"
+    },
+    {
+      id: 3,
+      title: "Metadata",
+      imgsrc: "/images/data_barru.jpg"
+  },
+]
