a7e20d4fa3f30a94dd5fc855ed1a4d656334c8a9
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-05-21 15:21:32 +0800

    fix: deleted mock data items

--- app/data-sektoral/page.tsx
+++ app/data-sektoral/page.tsx
@@ -2,30 +2,7 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import DataCard from "../components/dataCard";
-
-
-const dataSektoralItems = [
-    {
-        id: 1,
-        title: "Data Sektoral",
-        imgsrc: "/images/data_barru.jpg"
-    },
-    {
-        id: 2,
-        title: "Survey Sektoral",
-        imgsrc: "/images/data_barru.jpg"
-    },
-    {
-        id: 3,
-        title: "Kompilasi Produk Administrasi",
-        imgsrc: "/images/data_barru.jpg"
-    },
-    {
-      id: 3,
-      title: "Metadata",
-      imgsrc: "/images/data_barru.jpg"
-  },
-]
+import { dataSektoralItems } from "@/public/mockData/mockDataSektoral";
 
 function DataSektoral(){
   return(
