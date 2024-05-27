11ef04376e3f5302a5e564afbc22251af3ba6875
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-05-28 04:48:25 +0800

    feat: added fetch

--- app/components/PublicationCard/PublicationCard.tsx
+++ app/components/PublicationCard/PublicationCard.tsx
@@ -3,22 +3,20 @@ import Image from "next/image";
 import Link from "next/link";
 import textOverflow from "@/lib/textOverflow";
 
-interface PublicationCardProps {
-    id: string;
-    image: string;
-    title: string;
-    date: string;
-    views: number;
+function PublicationCard(data: any) {
+  let image = ""
+
+  if (data.data?.attributes?.image?.data?.attributes?.url) {
+    image = `http://localhost:3001${data.data.attributes.image.data.attributes.url}`
   }
 
-function PublicationCard(data: PublicationCardProps) {
   return (
     <Link 
-      href={"/publikasi/" + data.id}
+      href={`/publikasi/${data.data.id}`}
       className="flex flex-col border w-[23%] h-[60vh] rounded-3xl shadow-lg shadow-white-hover bg-white">
       <Image
         className="w-[100%] h-[70%] rounded-t-3xl"
-        src={data.image != "" ? data.image : ""}
+        src={image != "" ? image : ""}
         width={80}
         height={80}
         alt="Tidak ada Gambar"
@@ -26,14 +24,14 @@ function PublicationCard(data: PublicationCardProps) {
       <div>
         <div className="flex gap-2 mt-[2vh] h-[25%] pl-[5%]">
           <div className={`flex justify-center items-center w-[50%] text-xs text-white px-2 py-1 rounded-full bg-blue-light`}>
-            {data.date}
+            {"Date: " + data.data.attributes.tanggal_rilis}
           </div>
-          <div className={`flex justify-center items-center w-[40%] text-xs text-white px-2 py-1 rounded-full bg-amber-400`}>
-            {"Views: " + data.views}
+          <div className={`flex justify-center items-center w-[40%] text-xs text-white px-2 py-1 rounded-full bg-orange-dark`}>
+            {"Views: " + data.data.attributes.ISSN_ISBN}
           </div>
         </div>
         <h2 className="flex items-center font-bold text-blue-dark text-sm h-16 text-left px-[5%] mt-1">
-          {textOverflow(data.title, 75)}
+          {textOverflow(data.data.attributes.judul, 75)}
         </h2>
       </div>
     </Link>
