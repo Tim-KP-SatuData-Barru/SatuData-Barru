b93b2344893aa0a05cc6e1a71df7ca11b38ed679
Jessicawinardy <Jessicawinardy@gmail.com>
2024-05-29 01:32:11 +0800

    minor changes

--- app/components/statistik/Statistik.tsx
+++ app/components/statistik/Statistik.tsx
@@ -8,7 +8,7 @@ function Statistik() {
         <section className="flex flex-col">
           <p className="text-blue text-base">STATISTIK</p>
           <p className="text-orange font-bold text-2xl">
-            Statistik Satu Data Indonesia Tingkat Satu Data Kota Makassar
+            Statistik Satu Data Kabupaten Barru
           </p>
         </section>
       </section>
@@ -16,33 +16,29 @@ function Statistik() {
       <section className="flex flex-wrap grow gap-6">
         <div className="flex flex-col border w-[274px] h-[165px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white">
           <h2 className="flex font-bold text-blue-dark text-heading-l">
-            929.259
+            10.321
           </h2>
           <h3 className="text-orange font-bold text-heading-s">Element Data</h3>
           <h3 className="text-l">Kumpulan data yang dapat diakses publik</h3>
         </div>
 
         <div className="flex flex-col border w-[274px] h-[165px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white">
-          <h2 className="flex font-bold text-blue-dark text-heading-l">
-            980
-          </h2>
+          <h2 className="flex font-bold text-blue-dark text-heading-l">126</h2>
           <h3 className="text-orange font-bold text-heading-s">OPD/Instansi</h3>
           <h3 className="text-l">Data-Data Organisasi Perangkat Daerah</h3>
         </div>
 
         <div className="flex flex-col border w-[274px] h-[165px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white">
-          <h2 className="flex font-bold text-blue-dark text-heading-l">
-            20
-          </h2>
+          <h2 className="flex font-bold text-blue-dark text-heading-l">20</h2>
           <h3 className="text-orange font-bold text-heading-s">Kategori</h3>
           <h3 className="text-l">Kategori dalam website Satu Data Barru</h3>
         </div>
 
         <div className="flex flex-col border w-[274px] h-[165px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white">
-          <h2 className="flex font-bold text-blue-dark text-heading-l">
-            12.003.505
-          </h2>
-          <h3 className="text-orange font-bold text-heading-s">Jumlah Pengunjung</h3>
+          <h2 className="flex font-bold text-blue-dark text-heading-l">927</h2>
+          <h3 className="text-orange font-bold text-heading-s">
+            Jumlah Pengunjung
+          </h3>
           <h3 className="text-l">Jumlah Pengunjung Satu Data Barru</h3>
         </div>
       </section>
